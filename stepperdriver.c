e2ee8500905ddee2910faae712904bbb8317a144
Maxime ANDRE <maxime.andre@gadz.org>
2014-05-02 12:24:51 +0200

    Petites modifs

--- stepperdriver.c
+++ stepperdriver.c
@@ -11,34 +11,25 @@
 
 #define MOD(a,b) (((a)%(b)) < 0 ? ((a)%(b)+(b)) : ((a)%(b)))
 
-uint8_t half_step[] = {0b1110, 0b1100, 0b1101, 0b1001, 0b1011, 0b0011, 0b0111, 0b0110};
+//uint8_t half_step[] = {0b1110, 0b1100, 0b1101, 0b1001, 0b1011, 0b0011, 0b0111, 0b0110};
+uint8_t half_step[] = {0b0001, 0b0011, 0b0010, 0b0110, 0b0100, 0b1100, 0b1000, 0b1001};
 uint8_t c = 0;
-
-void forward(volatile uint8_t *p)
-{
-	*p = ((*p<<1) & 0xF) | ( (*p & 0xF) >> 3);
-}
-
-void backward(volatile uint8_t *p)
-{
-	*p = ((*p>>1) & 0xF) | ( (*p & 0xF) << 3);
-}
 
 void blink(uint8_t n) {
 	int i;
 	for(i=0;i<n;i++) {
 		PORTD = 0b1110;
 		_delay_ms(100);
-		PORTD = 0b1111;
+		PORTD = 0b0000;
 		_delay_ms(100);
 	}
 }
 
 ISR(PCINT0_vect) {
-	if((PINB & (1 << PINB0)) == 0) 
+	if(get(STEP) == 0) 
 		return;
 	if(DEBOUNCE) {
-		_delay_ms(100);
+		_delay_ms(200);
 	}
 	if(get(DIRECTION)) {
 		c = MOD(c+1, 8);
@@ -47,7 +38,6 @@ ISR(PCINT0_vect) {
 		c = MOD(c-1, 8);
 	}
 	PORTD=half_step[c];
-	//blink(10);
 }
 
 
